fa95446c1020b45f65397bb93dd69c13f2fdd8da
lincon-programmer <lincongallo@gmail.com>
2022-03-02 09:25:01 -0300

    init publish

---
+++ rabbit-queue-publisher-and-receiver.net/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace rabbit_queue_publisher_and_receiver.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var msgEnviar = new ObjetoPersonalizado { Id = Guid.NewGuid(), Mensagem = "Olá" + new Random().Next(1, 8).ToString() };
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+
+    public class ObjetoPersonalizado
+    {
+        public Guid Id { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
---
+++ rabbit-queue-publisher-and-receiver.net/Receptor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabbit_queue_publisher_and_receiver.net
+{
+    class Receptor
+    {
+    }
+}
